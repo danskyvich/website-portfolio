13de043ba68c9cf58a99d86bc7fdbe7a786f935c
Danilo Pelin <danilocanabanopelinjr@gmail.com>
2026-07-25 15:46:43 +0800

    ui: topbar ui finished

--- components/layout/TopBar.tsx
+++ components/layout/TopBar.tsx
@@ -31,12 +31,12 @@ const divToRight = {
 export default function TopBar() {
 
     const items = [
-        {name: "Home", link: "/"},
         {name: "Works", link: "/"},
         {name: "About Me", link: "/"},
     ]
 
     return (
+      /*
       <div className="flex bg-(--color-brand-purple) w-full h-fit justify-between items-center px-5 py-5 mt-5 ">
         <div className="flex gap-2.5 w-[50%] items-center justify-start">
           <Image src="favicon.svg" alt="alt" width={25} height={25} />
@@ -75,6 +75,26 @@ export default function TopBar() {
             </a>
             
           </motion.div>
+        </div>
+      </div>
+      */
+      <div className="flex w-full h-fit border-b border-(--color-line) px-10 2xl:px-[20%] py-3 bg-(--color-background) items-center justify-between">
+        {/* Left side */}
+        <div className="flex w-full h-full gap-10 text-[0.9rem] items-center font-">
+          <Image src="favicon.svg" alt="alt" width={45} height={45} />
+          <a href="" className="line-clamp-1">
+            My works
+          </a>
+          <a href="" className="line-clamp-1">
+            About me
+          </a>
+        </div>
+
+        {/* Right side */}
+        <div className="flex w-full h-full items-center justify-end gap-10">
+          <ButtonSlantedBg text="Resume" />
+
+          <ButtonSlantedBg text="Contacts" />
         </div>
       </div>
     );
